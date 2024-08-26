0f64d9970320dd72becf10b8eb7680961da77892
MancusoGit <150100922+MancusoGit@users.noreply.github.com>
2024-08-26 09:19:53 -0300

    Update desafio_binario.pas

--- recursion/desafio_binario.pas
+++ recursion/desafio_binario.pas
@@ -1,44 +1,23 @@
 program av;
 
-const
-
-	conv = 2;
-	
-type
-
-	lista = ^nodo;
-	
-	nodo = record
-		elem : integer;
-		sig : lista;
-	end;
-	
-//modulos
-
-	procedure agregarAdelante(var pi: lista; x : integer);
-	var
-		nue : lista;
-	begin
-		new(nue);
-		nue^.elem := x;
-		nue^.sig := pi;
-		pi := nue;
-	end;
-
-	procedure convertir(var x : integer; var pi : lista);
+	procedure convertir(x : integer);
 	begin
 		if (x <> 0) then begin
-			agregarAdelante(pi,x mod 2);
-			x := x div 2;
-			convertir(x,pi);
+			convertir(x div 2);
+			write(x mod 2);
 		end;
 	end;
 	
-	procedure imprimirlista(pi :lista);
+	procedure convirtiendoNumeros(num : integer);
 	begin
-		while(pi <> nil) do begin
-			write(pi^.elem);
-			pi := pi^.sig;
+		if (num <> 0) then begin
+			write(num,' en binario: ');
+			convertir(num);
+			writeln(' ');
+			writeln(' ');
+			write('ingrese un numero: ');
+			readln(num);
+			convirtiendoNumeros(num);
 		end;
 	end;
 	
@@ -47,18 +26,12 @@ type
 	
 	var
 	
-		num, x : integer;
-		pi : lista;
+		num : integer;
 		
 	begin
 	
 		write('ingrese un numero: ');
 		readln(num);
-		x := num;
-		writeln(' ');
-		pi := nil;
-		convertir(num,pi);
-		write(x,' en binario: ');
-		imprimirlista(pi);
-	
+		convirtiendoNumeros(num);
+		
 	end.
