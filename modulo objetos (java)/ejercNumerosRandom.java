def5f5c751cb57a99028e9a8dd7cdcf1b8aa2314
MancusoGit <150100922+MancusoGit@users.noreply.github.com>
2024-09-12 10:25:16 -0300

    Create ejercNumerosRandom.java

---
+++ modulo objetos (java)/ejercNumerosRandom.java
@@ -0,0 +1,23 @@
+import java.util.Random;
+
+public class NumRandom {
+
+    public static void main(String[] args) {
+        
+        final int FILTRO = 11; //declarando constante de corte
+        
+        int x;
+        
+        Random randomize = new Random(); 
+        
+        //Scanner scanner = new Scanner(System.in);
+        
+        x = randomize.nextInt(100); //generar en el rango de 0 - 100
+        
+        while (x != FILTRO) {
+            System.out.println("el numero generado es: " + x);
+            x = randomize.nextInt(100);
+        }
+        
+    }
+}
