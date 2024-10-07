44fbb3cdb482fadda8e5a044720ec8da62eb3409
MancusoGit <150100922+MancusoGit@users.noreply.github.com>
2024-10-07 09:19:23 -0300

    Update CoroSemicircular.java

--- modulo objetos (java)/Practica Repaso/Ejercicio4Practica5/src/ejercicio4practica5/CoroSemicircular.java
+++ modulo objetos (java)/Practica Repaso/Ejercicio4Practica5/src/ejercicio4practica5/CoroSemicircular.java
@@ -11,7 +11,53 @@ package ejercicio4practica5;
 public class CoroSemicircular extends Coro {
     
     private Corista [] coristas;
+    private final int CANTIDAD_CORISTAS;
+    private int dimlCoristas;
     
-    public CoroSemicir
+    public CoroSemicircular(String coroNombre, Director coroDirector, int cantidadCoristas) {
+        super(coroNombre, coroDirector);
+        this.coristas = new Corista [cantidadCoristas];
+        this.CANTIDAD_CORISTAS = cantidadCoristas;
+        this.dimlCoristas = 0;
+    }
     
+    public void agregarCorista(Corista nuevoCorista) {
+        if (dimlCoristas < CANTIDAD_CORISTAS) {
+            coristas[dimlCoristas] = nuevoCorista;
+            dimlCoristas++;
+        }
+        else {
+            System.out.println("coro lleno!");
+        }
+    }
+    
+    @Override
+    public boolean coroLleno() {
+        return (dimlCoristas == CANTIDAD_CORISTAS);
+    }
+    
+    @Override
+    public boolean coroOrdenado() {
+        int i = 1;
+        boolean orden = true;
+        if (this.coroLleno()) {
+            while ((i < CANTIDAD_CORISTAS) && (orden)) {
+                if (coristas[i-1].getTonoFundamental() > coristas[i].getTonoFundamental()) {
+                    orden = false;
+                }
+                else {
+                    i++;
+                }
+            }
+        }
+        return orden;
+    }
+    
+    public String toString() {
+        String aux = super.toString();
+        for (int i = 0; i < dimlCoristas; i++) {
+            aux += coristas[i].toString();
+        }
+        return aux;
+    }
 }
